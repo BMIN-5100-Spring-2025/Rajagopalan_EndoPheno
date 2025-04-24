b15591ae2ee570f86426271239477d6ac25814fb
Ananya Rajagopalan <ananya.rajagopalan@gmail.com>
2025-04-24 14:04:39 -0400

    added api connection

--- terraform/main.tf
+++ terraform/main.tf
@@ -162,3 +162,37 @@ resource "aws_ecs_task_definition" "rajagopalan-endopheno" {
     size_in_gib = 200
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "rajagopalan_endopheno_cors_configuration" {
+  bucket = aws_s3_bucket.rajagopalan-endopheno.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT", "HEAD"]
+    allowed_origins = ["http://localhost:5173", "bmin5100.com", "*.bmin5100"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+locals {
+  ecs_task_definition_container_name = "endopheno-project-container"
+}
+
+module "invoke_fargate_lambda" {
+  source = "git@github.com:BMIN-5100-Spring-2025/infrastructure.git//invoke_fargate_lambda/terraform?ref=f844e9c04f901768ccb99aff77286165bf71b83e"
+
+  project_name = "rajagopalan-endopheno"
+  ecs_task_definition_arn = aws_ecs_task_definition.rajagopalan-endopheno.arn
+  ecs_task_execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  ecs_task_task_role_arn = aws_iam_role.ecs_task_role.arn
+  ecs_task_definition_container_name = local.ecs_task_definition_container_name
+
+  ecs_cluster_arn = data.terraform_remote_state.infrastructure.outputs.ecs_cluster_arn
+  ecs_security_group_id = data.terraform_remote_state.infrastructure.outputs.ecs_security_group_id
+  private_subnet_id = data.terraform_remote_state.infrastructure.outputs.private_subnet_id
+  api_gateway_authorizer_id = data.terraform_remote_state.infrastructure.outputs.api_gateway_authorizer_id
+  api_gateway_execution_arn = data.terraform_remote_state.infrastructure.outputs.api_gateway_execution_arn
+  api_gateway_id = data.terraform_remote_state.infrastructure.outputs.api_gateway_id
+  environment_variables = {}
+}
