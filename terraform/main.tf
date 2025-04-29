db95ed64598e0ed3c65dd6a1c2f615626caea44a
Ananya Rajagopalan <ananya.rajagopalan@gmail.com>
2025-04-29 13:33:33 -0400

    added .com

--- terraform/main.tf
+++ terraform/main.tf
@@ -169,7 +169,7 @@ resource "aws_s3_bucket_cors_configuration" "rajagopalan_endopheno_cors_configur
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT", "HEAD"]
-    allowed_origins = ["http://localhost:5173", "bmin-5100.com", "*.bmin-5100"]
+    allowed_origins = ["http://localhost:5173", "bmin-5100.com", "*.bmin-5100.com"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
